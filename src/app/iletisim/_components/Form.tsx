34cd9e83862bcf92a02f4af053e9711f179528e2
Tunahan İPEK <tnhnipek@gmail.com>
2025-01-28 02:15:29 +0300

    contact form fix map

--- src/app/iletisim/_components/Form.tsx
+++ src/app/iletisim/_components/Form.tsx
@@ -44,14 +44,14 @@ const FormComponent = () => {
   return (
     <section className="text-gray-600 body-font relative">
       <div className="container sm:px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
-        <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg sm:overflow-hidden sm:mr-10 sm:p-10 flex items-end justify-start relative max-sm:mb-40">
+        <div className="lg:w-2/3 md:w-1/2 border rounded-lg sm:overflow-hidden sm:mr-10 sm:p-10 flex items-end justify-start relative max-sm:mb-40">
           <iframe
             width="100%"
             height="100%"
             className="absolute inset-0"
             title="map"
             src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=Ulusoy%20Apartmanı%20Karaağaç%20Firdevs%20Cd.%20No:%208%20D:14%2032100%20Merkez%20Isparta&ie=UTF8&t=&z=14&iwloc=B&output=embed"
-            style={{ filter: "grayscale(1) contrast(1.2) opacity(0.4)" }}
+            style={{ filter: "grayscale(.5) contrast(1.2) opacity(0.5)" }}
           ></iframe>
           <div className="bg-white relative flex flex-wrap py-6 rounded shadow-md max-sm:top-[50%] max-sm:left-1/2 max-sm:-translate-x-1/2 max-sm:w-4/5">
             <div className="lg:w-1/2 px-6">
