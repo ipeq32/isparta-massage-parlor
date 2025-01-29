ac840000dd0ca6a6a3f0d86dda1cff3c9e1ad43f
Tunahan İPEK <tnhnipek@gmail.com>
2025-01-29 20:16:43 +0300

    navbar responsivite action

--- src/app/_components/Navbar/index.tsx
+++ src/app/_components/Navbar/index.tsx
@@ -22,6 +22,105 @@ const NavbarComponent = () => {
     };
   }, []);
 
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 768) {
+        document.getElementById("mobile-menu")?.classList.add("hidden");
+      }
+    };
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  // mobile menu ve butonu dışında bir yere tıklandığında mobile menuyu kapat
+  useEffect(() => {
+    const handleClick = (e: MouseEvent) => {
+      if (
+        document.getElementById("mobile-menu")?.classList.contains("hidden")
+      ) {
+        return;
+      }
+
+      const target = e.target as HTMLElement;
+      if (
+        !target.closest("#mobile-menu") &&
+        !target.closest("#mobile-menu-button")
+      ) {
+        document.getElementById("mobile-menu")?.classList.add("hidden");
+      }
+    };
+    document.addEventListener("click", handleClick);
+
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  }, []);
+
+  const navbarLinks = () => {
+    return (
+      <>
+        <Link
+          href="/"
+          className={cn(
+            " hover:text-yellow-500 w-full text-nowrap text-center",
+            {
+              "text-yellow-500": pathname === "/",
+            }
+          )}
+        >
+          Ana Sayfa
+        </Link>
+        <Link
+          href="/hakkimizda"
+          className={cn(
+            "hover:text-yellow-500 w-full text-nowrap text-center",
+            {
+              "text-yellow-500": pathname === "/hakkimizda",
+            }
+          )}
+        >
+          Hakkımızda
+        </Link>
+        <Link
+          href="/iletisim"
+          className={cn(
+            "hover:text-yellow-500 w-full text-nowrap text-center",
+            {
+              "text-yellow-500": pathname === "/iletisim",
+            }
+          )}
+        >
+          İletişim
+        </Link>
+        <Link
+          href="/neden-biz"
+          className={cn(
+            "hover:text-yellow-500 w-full text-nowrap text-center",
+            {
+              "text-yellow-500": pathname === "/neden-biz",
+            }
+          )}
+        >
+          Neden Biz
+        </Link>
+        <Link
+          href="/galeri"
+          className={cn(
+            "hover:text-yellow-500 w-full text-nowrap text-center",
+            {
+              "text-yellow-500": pathname === "/galeri",
+            }
+          )}
+        >
+          Galeri
+        </Link>
+      </>
+    );
+  };
+
   return (
     <nav
       className={cn(
@@ -33,7 +132,7 @@ const NavbarComponent = () => {
         }
       )}
     >
-      <div className="container flex justify-between items-center">
+      <div className="container relative flex justify-between items-center">
         <div className="flex items-center space-x-4">
           <Link href="/">
             <Image
@@ -45,52 +144,13 @@ const NavbarComponent = () => {
           </Link>
         </div>
         <div
-          className={cn("flex text-lg items-center space-x-4", {
+          className={cn("flex text-lg items-center space-x-4 max-md:hidden", {
             "text-md": isScrolled,
           })}
         >
-          <Link
-            href="/"
-            className={cn(" hover:text-yellow-500", {
-              "text-yellow-500": pathname === "/",
-            })}
-          >
-            Ana Sayfa
-          </Link>
-          <Link
-            href="/hakkimizda"
-            className={cn("hover:text-yellow-500", {
-              "text-yellow-500": pathname === "/hakkimizda",
-            })}
-          >
-            Hakkımızda
-          </Link>
-          <Link
-            href="/iletisim"
-            className={cn("hover:text-yellow-500", {
-              "text-yellow-500": pathname === "/iletisim",
-            })}
-          >
-            İletişim
-          </Link>
-          <Link
-            href="/neden-biz"
-            className={cn("hover:text-yellow-500", {
-              "text-yellow-500": pathname === "/neden-biz",
-            })}
-          >
-            Neden Biz
-          </Link>
-          <Link
-            href="/galeri"
-            className={cn("hover:text-yellow-500", {
-              "text-yellow-500": pathname === "/galeri",
-            })}
-          >
-            Galeri
-          </Link>
+          {navbarLinks()}
         </div>
-        <div>
+        <div className="max-lg:hidden">
           <Link href="https://api.whatsapp.com/send/?phone=905302411232&text=Randevu%20almak%20istiyorum.">
             <button className="flex justify-between items-center gap-3 text-white font-semibold py-2 px-4 rounded hover:bg-gray-700 hover:text-green-500 transition-all duration-300 ease-linear">
               <Icon
@@ -100,6 +160,47 @@ const NavbarComponent = () => {
               <span>Randevu Al</span>
             </button>
           </Link>
+        </div>
+        <div className="md:hidden">
+          <button
+            id="mobile-menu-button"
+            className={cn(
+              "text-white font-semibold py-2 px-4 rounded hover:bg-gray-700 hover:text-green-500 transition-all duration-300 ease-linear",
+              {
+                "bg-gray-700 text-green-500": false, // buraya mobile menu açıkken icon rengi verilecek
+              }
+            )}
+            onClick={() => {
+              document
+                .getElementById("mobile-menu")
+                ?.classList.toggle("hidden");
+            }}
+          >
+            <Icon
+              icon="quill:hamburger-sidebar"
+              className="text-3xl cursor-pointer"
+            />
+          </button>
+          <div
+            id="mobile-menu"
+            className={cn("hidden absolute top-[90px] right-0 h-56", {
+              "top-16": isScrolled,
+            })}
+          >
+            <div className="flex flex-col items-center justify-center gap-y-2 h-full bg-gray-900 rounded-b-xl">
+              {navbarLinks()}
+              <Link
+                href="https://api.whatsapp.com/send/?phone=905302411232&text=Randevu%20almak%20istiyorum."
+                className="flex justify-between items-center gap-3 text-white font-semibold py-2 px-4 mt-2 rounded hover:bg-gray-700 hover:text-green-500 transition-all duration-300 ease-linear"
+              >
+                <Icon
+                  icon="logos:whatsapp-icon"
+                  className="w-6 h-6 inline-block"
+                />
+                <span>Randevu Al</span>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </nav>
